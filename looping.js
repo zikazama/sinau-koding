62602f2e7fac750513412b556cef6c09878a5482
zikazama <fauzi190198@gmail.com>
2024-10-31 21:01:38 +0700

    feat: calculator

---
+++ looping.js
@@ -0,0 +1,46 @@
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index);
+// }
+
+// let condition = 1;
+// while (condition) {
+//     console.log(condition);
+//     if (condition > 10) {
+//         break;
+//     }
+//     condition++;
+// }
+
+// const person = {"name":"taylor","age":31};
+// for (property in person) {
+// 	console.log(property,person[property]);
+// }
+
+// const array = [1,2,3,4,5];
+// array.forEach((element, index) => {
+// 	console.log(element, index);
+// });
+
+// function cetak(params) {
+//     if (params > 10) {
+//         return params+1;
+//     } else {
+//         return params-1;
+//     }
+// }
+
+// const nilai = cetak(1);
+// var x = 2;
+// var z = x+ nilai;
+// var nilai = 10;
+// console.log(z);
+
+// console.log(x); // Output: undefined
+// var x = 5;
+// console.log(x); // Output: 5
+
+sayHello(); // Output: "Hello!"
+
+function sayHello() {
+    console.log("Hello!");
+}
